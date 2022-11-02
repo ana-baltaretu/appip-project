ffb76b3fd63d236d89de22de541a6ae8f10c4359
Ana Băltărețu <a.baltaretu@student.tudelft.nl>
2022-11-02 19:57:08 +0100

    Add finding correct nodes for editing

--- ana_src/main.py
+++ ana_src/main.py
@@ -83,6 +83,8 @@ if __name__ == '__main__':
     # Check shape and maybe fixes
     final_image = cv2.imread(final_applied_maps)
     w1, h1, c1 = resulting_image.shape
+
+    print("Inpute these in Blender: " + str(w1) + ", " + str(h1))
     w2, h2, c2 = final_image.shape
     apply_textured_object(input_image, final_image, mask_image)
 
--- ana_src/my_blender_scripts.py
+++ ana_src/my_blender_scripts.py
@@ -3,6 +3,32 @@ from bpy.types import Operator
 from bpy.props import FloatVectorProperty
 from bpy_extras.object_utils import AddObjectHelper, object_data_add
 from mathutils import Vector
+import subprocess
+import sys
+import os
+
+# TODO: HERE INPUT WIDTH AND HEIGHT
+W, H = 1877, 1282
+
+# path to python.exe
+#python_exe = os.path.join(bpy.app.binary_path)
+ 
+# upgrade pip
+#subprocess.call([python_exe, "-m", "ensurepip"])
+#subprocess.call([python_exe, "-m", "pip3 install opencv-python"])
+ 
+# install required packages
+#subprocess.call([python_exe, "-m", "pip", "install", "opencv-python"])
+#import cv2
+
+
+
+# IMPORTING CODE FROM ANOTHER FILE!!
+#filename = os.path.join(os.path.dirname(bpy.data.filepath), "my_configuration.py")
+#exec(compile(open(filename).read(), filename, 'exec'))
+
+bpy.context.scene.render.resolution_x = H
+bpy.context.scene.render.resolution_y = W
 
 # This is the actual line of code that adds the cube.
 # Magic numbers for rotating 90 degrees, DO NOT CHANGE PLEASE GOD!
@@ -50,3 +76,25 @@ set_material_to_face("Imported_Material", 2)
 bpy.ops.uv.reset()
 
 bpy.ops.object.mode_set(mode='OBJECT')
+
+textured_image_name = "nefertitti_waves.png"
+bpy.ops.image.open(filepath="//..\\output\\applied_textures\\" + textured_image_name, directory="C:\\Users\\anaba\\OneDrive\\Desktop\\Q1\\_Applied_Image_Processing\\Assignments\\project\\appip-project\\output\\applied_textures\\", files=[{"name":textured_image_name, "name":textured_image_name}], relative_path=True, show_multiview=False)
+
+
+mat = bpy.data.materials.get("Imported_Material")
+for k,v in mat.node_tree.nodes.items():
+#    print(k)
+    if "Image Texture" in k and v.label == "NORMAL_MAP_INPUT":
+        print("This is normal map", k, v.label)
+    if "Image Texture" in k and v.label == "SPECULARITY_INPUT":
+        print("This is specularity map", k, v.label)
+    if "Image Texture" in k and v.label == "SOURCE_INPUT_IMAGE":
+        print("This is input image", k, v.label)
+        #bpy.data.materials["Imported_Material"].node_tree.nodes[k].label = "aaaaaaaaaaaaaaaaaaa"
+
+        
+#    print(v)
+
+#bpy.ops.node.add_file(filepath="C:\\Users\\anaba\\OneDrive\\Desktop\\Q1\\_Applied_Image_Processing\\Assignments\\project\\appip-project\\input\\images\\nefertitti_no_background.png")
+#bpy.ops.node.link(detach=False, has_link_picked=False, drag_start=(-137.003, -778.387))
+
